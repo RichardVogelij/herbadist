d5ae974445c24bbd3ca8528b6ec390876c1a3690
richard <richard@vogelij.nl>
2016-01-04 20:45:53 +0100

    new template

---
+++ files/skin.php
@@ -0,0 +1,27 @@
+
+.toggle-label { background-image: url("cyan/toggle-button-plus.png"); }
+
+.active-toggle .toggle-label { background-image: url("cyan/toggle-button-minus.png"); }
+
+.pricing-table .plan-details li i.tick { background-image: url(cyan/tick.png); }
+
+.pricing-table .plan-details li i.cross { background-image: url(cyan/cross.png); }
+
+.contact-form .button i.send { background-image: url(cyan/send-icon.png); }
+    
+#featured-sources .heading2 .title { background-image: url(cyan/featured-icon.png)}
+
+.tp-bullets.simplebullets.round .bullet { background-image: url(cyan/bullet.png); }
+
+@media only screen and (-webkit-min-device-pixel-ratio: 2) {
+
+    .pricing-table .plan-details li i.tick { background-image: url(cyan/tick@2x.png); }
+
+    .pricing-table .plan-details li i.cross { background-image: url(cyan/cross@2x.png); }
+
+    .contact-form .button i.send { background-image: url(cyan/send-icon@2x.png);}
+
+    #featured-sources .heading2 .title { background-image: url(cyan/featured-icon@2x.png)}
+
+}
+
